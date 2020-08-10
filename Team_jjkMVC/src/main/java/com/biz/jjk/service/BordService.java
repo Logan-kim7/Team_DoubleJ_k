106d007a8108cafdffcb5fd0f1dffbcf2110d82d
skynate22 <skynate22@naver.com>
2020-08-10 16:01:00 +0900

    first Project

---
+++ Team_jjkMVC/src/main/java/com/biz/jjk/service/BordService.java
@@ -0,0 +1,11 @@
+package com.biz.jjk.service;
+
+public interface BordService {
+	
+	public String strRank();
+	public String strLgd();
+	public String strWord();
+	public String strBlList();
+	
+
+}
