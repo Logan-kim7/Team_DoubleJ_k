47d7df6fa767542414dbf1a483834a0f88619c48
h2ljh <h2ljh@naver.com>
2020-10-22 01:53:23 +0900

    index page 수정

--- src/main/java/com/biz/dripbag/controller/UserController.java
+++ src/main/java/com/biz/dripbag/controller/UserController.java
@@ -6,11 +6,15 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.biz.dripbag.model.UserVO;
 import com.biz.dripbag.service.SessionService;
@@ -22,18 +26,29 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping(value = {"/user", "/user/"})
 @Controller
 public class UserController 
-{		
-	private final UserService uService;
-	private final SessionService sService;
+{	
+	@Autowired
+	@Qualifier("UserServiceV2")
+	private UserService uService;
 	
-	@RequestMapping(value ={"/check", "/check/"}, method=RequestMethod.POST)
-	public String check(@ModelAttribute("userVO") UserVO userVO, Model model, HttpServletRequest req, HttpServletResponse res) throws IOException
+	@Autowired
+	final SessionService sService;
+	
+	@ResponseBody
+	@RequestMapping(value ="/check/{id}", method=RequestMethod.GET)
+	public String check(@PathVariable("id") String id)
 	{
-		if(uService.checkByUser(userVO, 2))
-			sService.sessionRegistration(req, userVO);
-		
-		else
-			sService.interceptorLogin(res, "로그인또는 비밀번호 일치 하지 않음");
+		return "aa";
+	}
+	@RequestMapping(value ={"/check", "/check/"}, method=RequestMethod.POST)
+	public String check(UserVO userVO, Model model, HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		System.out.println(userVO.getU_email());
+//		if(uService.checkByUser(userVO, 2) == 2)
+//			sService.sessionRegistration(req, userVO);
+//		
+//		else
+//			sService.interceptorLogin(res, "로그인또는 비밀번호 일치 하지 않음");
 		
 		return "redirect:/main/";
 	}
@@ -44,15 +59,19 @@ public class UserController
 	public void PostJoin(@ModelAttribute("userVO") UserVO userVO, Model model, HttpServletResponse res) throws IOException
 	{		
 		String alertText = "";
-		if(uService.checkByUser(userVO, 1) == false)
+		int flag = uService.checkByUser(userVO, 1);
+		
+		if(flag == -1)
+			alertText = "공백 확인 해주세요.";
+		
+		else if(flag == 1)
 		{
-			uService.join(userVO);
+			uService.insert(userVO);
 			alertText = "회원가입 완료";
 		}
 		else { alertText = "중복된 아이디 입니다."; }
 		
-		sService.interceptorLogin(res, alertText);
-			
+		sService.interceptorLogin(res, alertText);		
 	}
 	
 	
--- src/main/java/com/biz/dripbag/service/impl/UserServiceV1.java
+++ src/main/java/com/biz/dripbag/service/impl/UserServiceV1.java
@@ -2,8 +2,10 @@ package com.biz.dripbag.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.biz.dripbag.mapper.UserDAO;
 import com.biz.dripbag.model.UserVO;
 import com.biz.dripbag.service.UserService;
 
@@ -14,77 +16,46 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserServiceV1 implements UserService 
 {
-	private final List<UserVO> userList;
-		
-	@Override
-	public void join(UserVO vo)
-	{ this.userList.add(vo); }
-		
+	@Autowired
+	protected UserDAO userDAO;
+	
 	@Override
 	public List<UserVO> selectAll() 
 	{
-		return null;
+		return userDAO.selectAll();
 	}
 
 	@Override
 	public UserVO findById(Long id) 
 	{
-		return null;
+		return userDAO.findById(id);
 	}
 
 	@Override
 	public int insert(UserVO vo) 
 	{
-		return 0;
+		int ret = userDAO.insert(vo);
+		return ret;
 	}
 
 	@Override
 	public int update(UserVO vo) 
 	{
-		return 0;
+		int ret = userDAO.update(vo);
+		return ret;
 	}
 
 	@Override
 	public int delete(Long id) 
 	{
-		return 0;
+		int ret = userDAO.delete(id);
+		return ret;
 	}
 	
-	
 	@Override
-	public boolean checkByUser(UserVO vo, int check) 
+	public int checkByUser(UserVO vo, int check) 
 	{
-		UserVO reqVO = vo;	
-		/* List<UserVO> userList = selectAll(); */
-		String[] strCheck = new String[] { vo.getUserId(), vo.getUserPassword() };
-			
-
-		 if(userList.size() == 0)  
-			 return false;
-
-		switch (check) 
-		{
-			case -2:
-				for(int i= 0; i < strCheck.length; ++i) // 공백 널값 체크
-				{
-					strCheck[i] = strCheck[i].trim();
-					if(strCheck[i].isEmpty())
-						return true;
-				}
-		
-			case 1: // 아이디 중복 체크
-				for(int i= 0; i < userList.size(); ++i)
-					if(userList.get(i).getUserId().equals(reqVO.getUserId())) 
-							return true;
-				
-			case 2: // 로그인 체크
-				for(int i= 0; i < userList.size(); ++i)
-					if( userList.get(i).getUserId().equals(reqVO.getUserId()) && 
-					    userList.get(i).getUserPassword().equals(reqVO.getUserPassword())
-					   ) 
-						return true;
-			}
-		return false;
+		return 0;
 	}
 
 }
