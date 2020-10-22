c614311afc7c11413fb58466be7c1f4661550c8f
h2ljh <h2ljh@naver.com>
2020-10-22 23:21:22 +0900

    정현

--- src/main/java/com/biz/dripbag/crawling/CrwalingData.java
+++ src/main/java/com/biz/dripbag/crawling/CrwalingData.java
@@ -15,8 +15,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.biz.dripbag.model.GoogleVO;
+import com.biz.dripbag.model.NewsVO;
 import com.biz.dripbag.service.DateService;
 import com.biz.dripbag.service.GoogleTrendeService;
+import com.biz.dripbag.service.NewsService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,15 +32,23 @@ public class CrwalingData
 	@Qualifier("GoogleServiceV1")
 	private final GoogleTrendeService googleService;
 	
+	@Qualifier("NewsServiceV1")
+	private final NewsService newsService;
+	
 	private final DateService dateService;
 	
+	private final List<GoogleVO> googleList;
+	private final List<NewsVO> newsList;
 	
-	//@Scheduled(fixedDelay = 10000)
-	public void googleTrend() 
+	
+	@Scheduled(fixedDelay = 10000)
+	public List<GoogleVO> googleTrend() 
 	{
+		googleList.clear();
 		String url = "https://trends.google.co.kr/trends/trendingsearches/daily/rss?geo=KR";
 		Document doc;
-		GoogleVO vo; 
+		GoogleVO vo;
+		int index = 0;
 		try 
 		{
 		  doc = Jsoup.connect(url).get();
@@ -47,10 +57,14 @@ public class CrwalingData
 		  for(Element one : trendList) 
 		  { 
 			  vo = new GoogleVO(); 
-			  vo.setGtrand_title(one.select("title").text());
-			  vo.setGtrand_img(one.getElementsByTag("ht:picture").text());
-			  vo.setGtrand_date(dateService.dateTime()[0]);
-			  //googleService.insert(vo);
+			  vo.setGt_title(one.select("title").text());
+			  vo.setGt_img(one.getElementsByTag("ht:picture").text());
+			  vo.setGt_date(dateService.dateTime()[0]);
+//			  googleService.insert(vo);
+			  googleList.add(vo);
+			  ++index;
+			  if(index>= 10)
+				  break;
 		  }
 		  
 		} 
@@ -58,17 +72,22 @@ public class CrwalingData
 		{
 			System.out.println("구글 트렌드 접속 실패 URL 확인");
 			e.printStackTrace();
-		} 
+		}
+		
+		index=0;
+		return googleList;
 	}
 	
 	
-//	@Scheduled(cron = "0/10 * * * * *")
-	public void news()
+	@Scheduled(fixedDelay = 10000)
+	public List<NewsVO> news()
 	{
-		String[] detailNews = null;
-		String[] newsData = new String[3];
+		newsList.clear();
 		String newsUrl = "/?pageIndex=0"; // 뉴스 페이지 인덱스 문자열
-		String url = "https://mnews.sarangbang.com"; // 뉴스페이지 URL ( 디테일 붙이기 용도) 
+		String url = "https://mnews.sarangbang.com"; // 뉴스페이지 URL ( 디테일 붙이기 용도)
+		
+		NewsVO vo;
+		String[] detailNews;
 		int index = 0;
 		
 		try 
@@ -84,19 +103,20 @@ public class CrwalingData
 				System.out.println(detailNews[index]);
 				index++;
 			}
-			newsList.clear();
 			
+			newsList.clear();
 			index = 0;
+			
 			for(String one : detailNews)
 			{
 				doc = Jsoup.connect(detailNews[index]).get();
-				newsData[0] = doc.select(".view_wrap > .article_head h3").text(); // Title
-				newsData[1] = doc.select(".figcaption.text-center img").attr("src"); // img url
-				newsData[2] = doc.select(".article_view p").text(); // content
+				
+				vo = new NewsVO();
+				vo.setN_title(doc.select(".view_wrap > .article_head h3").text()); // Title
+				vo.setN_img(doc.select(".figcaption.text-center img").attr("src")); // img url
+				vo.setN_content(doc.select(".article_view p").text()); // content									
+				//newsService.insert(vo);
 			
-				System.out.println(newsData[0]);
-				System.out.println(newsData[1]);
-				System.out.println(newsData[2]);					
 				index++;
 			}			
 		}
@@ -106,6 +126,8 @@ public class CrwalingData
 			System.out.println("사랑방 뉴스 접속 실패 URL 확인");
 			e.printStackTrace();
 		}
+		
+		return newsList;
 	}
 		
 }
