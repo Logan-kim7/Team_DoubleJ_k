d337edd02a15971d88a689fe7fc000d37dfcafc4
JtoJ00721 <moonlf2000@naver.com>
2020-11-02 20:46:32 +0900

    수정

--- src/main/java/com/biz/dripbag/mapper/GoogleRankingDAO.java
+++ src/main/java/com/biz/dripbag/mapper/GoogleRankingDAO.java
@@ -2,7 +2,6 @@ package com.biz.dripbag.mapper;
 
 import com.biz.dripbag.model.GoogleRankingVO;
 
-public interface GoogleRankingDAO extends GenericDAO<GoogleRankingVO, Long> 
-{
+public interface GoogleRankingDAO extends GenericDAO<GoogleRankingVO, Long> {
 
 }
