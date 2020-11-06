64ada2ab5425dfd9e4e210b2a66597ac048a642d
JtoJ00721 <moonlf2000@naver.com>
2020-11-06 09:12:37 +0900

    import clean

--- src/main/java/com/biz/dripbag/controller/SearchController.java
+++ src/main/java/com/biz/dripbag/controller/SearchController.java
@@ -4,12 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.xml.stream.Location;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/com/biz/dripbag/controller/UserController.java
+++ src/main/java/com/biz/dripbag/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.biz.dripbag.controller;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
